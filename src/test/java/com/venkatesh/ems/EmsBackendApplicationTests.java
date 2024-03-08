bae69642892226c57690bea5b6ad4ac28b3a39c1
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-03-08 08:56:57 -0500

    init setup

---
+++ src/test/java/com/venkatesh/ems/EmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venkatesh.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
